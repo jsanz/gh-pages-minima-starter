de89a4cbed613122f4ccc71e92d46460b2affbe0
Arawn Davies <arawn.davies@gmail.com>
2022-04-10 22:25:36 +0100

    Create sc-126.d

---
+++ projects/hardware/sc-126.d
@@ -0,0 +1,11 @@
+---
+permalink: /projects/hardware/z80-mbc2
+layout: page
+title: Projects
+---
+
+# SC-126 Retro Computer with Z180, SD Card, digital IO and more
+
+placeholder text, full blog entry coming soon...
+
+https://www.tindie.com/products/tindiescx/sc126-z180-sbc-motherboard-kit/
